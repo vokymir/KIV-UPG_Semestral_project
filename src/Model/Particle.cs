517cb7e4c59c6bcef466014687b6cdbce6ba9c46
Vokymir <voky.consultant@gmail.com>
2024-11-15 11:59:11 +0100

    trying dynamic scenarios, but blind way

--- src/Model/Particle.cs
+++ src/Model/Particle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Flee.PublicTypes;
 
 namespace ElectricFieldVis.Model
 {
@@ -14,13 +15,45 @@ namespace ElectricFieldVis.Model
         // now I know I could use Vector2, but instead I have these two attributes X,Y
         public float X {  get; set; }
         public float Y { get; set; }
-        public float Value { get; set; }
 
-        public Particle(float x, float y, float value)
+        public float _time = 0;
+        private ExpressionContext _context;
+        private IGenericExpression<double>? _expr;
+
+        private string EXPRESSION = "1";
+
+        private float _value = 0;
+        public float Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_expr == null)
+                {
+                    _expr = _context.CompileGeneric<double>(EXPRESSION);
+                }
+                _time = value;
+                _context.Variables["t"] = _time;
+                _value = (float)_expr.Evaluate();
+            }
+        }
+
+
+        public Particle(float x, float y, string expression)
         {
             X = x;
             Y = y;
-            Value = value;
+            EXPRESSION = expression;
+            _context = new ExpressionContext();
+            _expr = _context.CompileGeneric<double>(expression);
+            
+        }
+        public Particle()
+        {
+            _context = new ExpressionContext();
         }
     }
 }
