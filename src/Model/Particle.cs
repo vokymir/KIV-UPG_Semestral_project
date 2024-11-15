73d7e6e4ecf226ee03599eb7eb80f46969f68f17
Vokymir <voky.consultant@gmail.com>
2024-11-15 12:36:37 +0100

    particle loading value and expression in progress

--- src/Model/Particle.cs
+++ src/Model/Particle.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Flee.PublicTypes;
 
 namespace ElectricFieldVis.Model
 {
@@ -14,13 +16,65 @@ namespace ElectricFieldVis.Model
         // now I know I could use Vector2, but instead I have these two attributes X,Y
         public float X {  get; set; }
         public float Y { get; set; }
-        public float Value { get; set; }
+        public float Value { 
+            get 
+            { 
+                return Value;
+            }
 
-        public Particle(float x, float y, float value)
+            set 
+            { 
+                if (_expr == null)
+                {
+                    Value = value;
+                }
+                else
+                {
+                    Value = (float)_expr.Evaluate();
+                }
+            } 
+        }
+        public string Expression {
+            get
+            {
+                return Expression;
+            }
+            set 
+            { 
+                if (_context == null)
+                {
+                    _context = new ExpressionContext();
+                    _context.Variables["t"] = 0;
+                }
+                // predpokladam spravnost vstupu
+                Expression = value;
+
+                _expr = _context.CompileGeneric<double>(Expression);
+                Value = (float)_expr.Evaluate();
+            }
+        }
+        
+        private ExpressionContext? _context;
+        private IGenericExpression<double>? _expr;
+        
+
+        public Particle(float x, float y, float val, string exp )
         {
             X = x;
             Y = y;
-            Value = value;
+            Expression = exp;
+            Value = val;
+            
+            _context = new ExpressionContext();
+            _context.Variables["t"] = 0;
+            _expr = _context.CompileGeneric<double>(exp);
         }
+        
+        public void setValue(float time)
+        {
+            _context.Variables["t"] = time;
+            Value = (float)(_expr.Evaluate());
+        }
+        public Particle() { Expression = ""; }
     }
 }
