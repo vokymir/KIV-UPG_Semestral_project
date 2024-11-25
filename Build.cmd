4fa352d68e6fe1d1ffcaa9983559492c4120fffc
Vokymir <voky.consultant@gmail.com>
2024-11-25 09:15:14 +0100

    legend maybe done and starting graphs

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,7 @@
 @mkdir .\bin
 cd src
 echo y | dotnet add package Flee
+echo y | dotnet add package Scottplot
 cd ..
 echo a | dotnet msbuild ./src/ElectricFieldVis.sln -t:Rebuild -p:Configuration=Release
 echo a | xcopy .\src\bin\Release\net8.0-windows\*.* .\bin /S
